0f022c50d0fa7407fdb1c87465b39f4f3391aaa6
DrPoppyseed <peaske16180@gmail.com>
2022-11-12 14:00:03 +0900

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+struct Cli {
+    pattern: String,
+    path:    std::path::PathBuf,
+}
+
+pub fn main() {
+    let pattern = std::env::args().nth(1).expect("no pattern given");
+    let path = std::env::args().nth(2).expect("no path given");
+}
